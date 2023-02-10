337c8799685cc66c78e8dcd395bc817783aceeaa
Vyas Raina <42746548+rainavyas@users.noreply.github.com>
2023-02-10 17:14:59 +0000

    Update train.py

--- train.py
+++ train.py
@@ -13,9 +13,9 @@ logger = logging.getLogger(__name__)
 # Model arguments
 model_parser = argparse.ArgumentParser(description='Arguments for system and model configuration')
 model_parser.add_argument('--path', type=str, help='path to experiment', required=True)
-model_parser.add_argument('--transformer', type=str, default='bert-base', help='[bert, roberta, electra ...]')
+model_parser.add_argument('--transformer', type=str, default='roberta-large', help='[bert, roberta, electra ...]')
 model_parser.add_argument('--prompt-finetuning', action='store_true', help='whether to use prompt finetuning')
-model_parser.add_argument('--label-words', type=str, nargs = '+', default=None, help='for prompt finetuning, which words to use as the labels')
+model_parser.add_argument('--label-words', type=str, nargs = '+', default=['terrible', 'great'], help='for prompt finetuning, which words to use as the labels')
 
 model_parser.add_argument('--maxlen', type=int, default=512, help='max length of transformer inputs')
 model_parser.add_argument('--num-classes', type=int, default=2, help='number of classes (3 for NLI)')
@@ -23,7 +23,7 @@ model_parser.add_argument('--rand-seed', type=int, default=None, help='random se
 
 ### Training arguments
 train_parser = argparse.ArgumentParser(description='Arguments for training the system')
-train_parser.add_argument('--dataset', type=str, default='snli', help='dataset to train the system on')
+train_parser.add_argument('--dataset', type=str, default='sst', help='dataset to train the system on')
 train_parser.add_argument('--bias', type=str, default=None, help='whether data should be synthetically biased (e.g. lexical)')
 train_parser.add_argument('--bias-bounds', type=float, nargs = '+', default=None, help='where the bias groups should be splits')
 
@@ -31,7 +31,7 @@ train_parser.add_argument('--lim', type=int, default=None, help='size of data su
 
 train_parser.add_argument('--epochs', type=int, default=3, help='number of epochs to train system for')
 train_parser.add_argument('--bsz', type=int, default=16, help='training batch size')
-train_parser.add_argument('--lr', type=float, default=2e-5, help='learning rate')
+train_parser.add_argument('--lr', type=float, default=1e-5, help='learning rate')
 train_parser.add_argument('--data-ordering', action='store_true', help='dynamically batches to minimize padding')
 
 train_parser.add_argument('--grad-clip', type=float, default=1, help='gradient clipping')
